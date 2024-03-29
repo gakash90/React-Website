4607893b0278adda503c35af62d84787602012fb
gakash90 <143600876+gakash90@users.noreply.github.com>
2024-03-30 00:51:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './public/**/*.html',
+    './public/**/*.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
